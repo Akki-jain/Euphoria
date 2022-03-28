b424246317f48812a9bddba88bcb6acc96748fbf
Akshat Jain <jainakshat75@gmail.com>
2022-03-28 08:12:29 +0530

    Addes contact.php and made some minor changes

---
+++ contact.php
@@ -0,0 +1,21 @@
+<?php
+
+include('connect.php');
+
+$name=$_POST['name'];
+$email=$_POST['email'];
+$number=$_POST['number'];
+$message=$_POST['subject'];
+
+
+$sql1 = "INSERT INTO helpdesk VALUES ('$name','$email','$number','$message')";
+
+if ($con->query($sql1) === TRUE) 
+{
+ echo "New record created successfully<BR><BR><BR>";
+} 
+else 
+{
+ echo "Error: " . $sql . "<br>" . $conn->error;
+}
+?>
